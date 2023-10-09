25c9d5a8054c92fafb8a24e6973341e12ee95b0f
Galeiro <galihsaputra1234@gmail.com>
2023-10-09 17:49:26 +0700

    ubah: tag input

--- pages/password-protect.tsx
+++ pages/password-protect.tsx
@@ -13,7 +13,7 @@ const PasswordProtectPage = () => {
               <path fill-rule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clip-rule="evenodd"/>
             </svg> */}
               <form action="/api/password-protect" method="post">
-                <input className="pl-2 flex flex-col outline-none border-none " type="text" placeholder="Password" />
+                <input className="pl-2 flex flex-col outline-none border-none " type="text" name="password" placeholder="Password" />
                 <button type="submit" className="block w-full bg-indigo-600 mt-4 py-2 rounded-2xl text-white font-semibold mb-2">Login</button>
                 {error && (
                   <span className="text-xs my-10 text-error">{error}</span>
